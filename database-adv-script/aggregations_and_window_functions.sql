129692fb9f7a8d27bf2034a11f52c6ee37e9122f
alidrisy <a.alidrisy6@gmail.com>
2025-05-16 22:59:38 +0300

    Add user booking count and ranking queries to aggregations and window functions script

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,1 +1,14 @@
-SELECT COUNT
+SELECT user_id, COUNT(*) FROM Booking GROUP BY user_id;
+
+SELECT
+    booking_id,
+    property_id,
+    ROW_NUMBER() OVER (
+        PARTITION BY
+            property_id
+    ) AS row_num,
+    RANK() OVER (
+        PARTITION BY
+            property_id
+    ) AS property_rank
+FROM Booking;
