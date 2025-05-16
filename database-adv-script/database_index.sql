d3ace3d3c22006243f90a48a0a90abec90d2d75a
alidrisy <a.alidrisy6@gmail.com>
2025-05-16 23:45:55 +0300

    Add new indexes for Booking and Review tables and include EXPLAIN query for user-specific booking retrieval

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -6,8 +6,16 @@ CREATE INDEX idx_booking_property_id ON Booking (property_id);
 
 CREATE INDEX idx_booking_user_id ON Booking (user_id);
 
+-- DROP INDEX idx_booking_user_id ON Booking;
+
 CREATE INDEX idx_payment_booking_id ON Payment (booking_id);
 
 CREATE INDEX idx_review_property_id ON Review (property_id);
 
 CREATE INDEX idx_review_user_id ON Review (user_id);
+
+EXPLAIN
+SELECT *
+FROM Booking
+WHERE
+    user_id = "11111111-1111-1111-1111-111111111111";
