99f800398b0dd1e4790cefd47b1bfe1f4a1dccb0
alidrisy <a.alidrisy6@gmail.com>
2025-05-16 22:29:08 +0300

    Add ORDER BY clause to JOIN query for Property and Review

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -4,7 +4,8 @@ from Booking
 
 SELECT *
 FROM Property
-    LEFT JOIN Review ON Property.property_id = Review.property_id;
+    LEFT JOIN Review ON Property.property_id = Review.property_id
+ORDER BY Property.name;
 
 SELECT *
 FROM users FULL OUTER
