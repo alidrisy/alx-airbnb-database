59c0e9757162e262693f0f80eb7a0401aaa316ef
Abdulrahman Alidrisy <a.alidrisy6@gmail.com>
2025-05-17 11:39:11 +0300

    Optimize JOIN performance by adding necessary indexes and refining queries

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,15 @@
+-- Initial query
+SELECT
+  *
+FROM Booking b
+JOIN User u ON b.user_id = u.user_id
+JOIN Property p ON b.property_id = p.property_id
+JOIN Payment pay ON b.booking_id = pay.booking_id;
+
+-- explain this
+EXPLAIN SELECT
+  *
+FROM Booking b
+JOIN User u ON b.user_id = u.user_id
+JOIN Property p ON b.property_id = p.property_id
+JOIN Payment pay ON b.booking_id = pay.booking_id;
