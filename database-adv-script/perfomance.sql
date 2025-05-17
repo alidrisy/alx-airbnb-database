1deeb5f2fbad19e319d10d9551d2ae78a240e57f
Abdulrahman Alidrisy <a.alidrisy6@gmail.com>
2025-05-17 11:42:38 +0300

    Refactor SQL queries for JOIN performance optimization and user-specific data retrieval

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,15 +1,21 @@
 -- Initial query
+-- Initial query
 SELECT
   *
 FROM Booking b
 JOIN User u ON b.user_id = u.user_id
 JOIN Property p ON b.property_id = p.property_id
-JOIN Payment pay ON b.booking_id = pay.booking_id;
+JOIN Payment pay ON b.booking_id = pay.booking_id
+WHERE u.email = 'alice@example.com' AND p.name = 'Cozy Cabin';
+
+
 
 -- explain this
-EXPLAIN SELECT
+EXPLAIN -- Initial query
+SELECT
   *
 FROM Booking b
 JOIN User u ON b.user_id = u.user_id
 JOIN Property p ON b.property_id = p.property_id
-JOIN Payment pay ON b.booking_id = pay.booking_id;
+JOIN Payment pay ON b.booking_id = pay.booking_id
+WHERE u.email = 'alice@example.com' AND p.name = 'Cozy Cabin';
